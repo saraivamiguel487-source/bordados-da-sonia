817f1a08176dbf02364b93a1946aa58c9dc0bd23
Miguel Saraiva Almeida <miguel62028446@edu.df.senac.br>
2026-05-14 17:30:50 -0300

    You brought here to kill me

--- js/busca.js
+++ js/busca.js
@@ -1,3 +1,45 @@
 const formBusca = document.querySelector(".busca")
 const campoBusca = document.querySelector(".busca__campo")
-const itemCatalogos = document.querySelectorAll(".bordados")
+const itensCatalogo = document.querySelectorAll(".bordados")
+
+//Verificar se o formulário de busca existe na página
+
+if (formBusca) {
+ formBusca.addEventListener("submit", function (event) {
+   event.preventDefault();
+ })
+
+}
+
+// agora vamos verivicar o texto que foi digitado
+
+campoBusca.addEventListener("input", function () {
+
+
+
+
+// converter o texo para minúsculo para facilitar a comparação
+
+const termoBusca = campoBusca.value.toLowerCase()
+
+// vamos verificar todas as seceções de bordados
+
+itensCatalogo.forEach(function(item) {
+
+// pegar o elemento que tem o texto bordado
+ const titulo = item.querySelector('h3').innerText.toLowerCase();
+// verificar se o termo de busca é igual ao título
+ if (titulo.includes(termoBusca)) {
+   item.style.display = 'block'; //Mostrar o item
+
+
+ } else {
+  item.style.display = 'none'; // Ocultar o item
+ }
+
+
+
+
+})
+
+})
