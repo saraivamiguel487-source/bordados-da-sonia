367fa6ed2277863f5429395319d5b9ac607dd01c
Miguel Saraiva Almeida <miguel62028446@edu.df.senac.br>
2026-05-18 17:39:21 -0300

    May the 18th be with you.

---
+++ js/Carrinho.js
@@ -0,0 +1,38 @@
+//Pega os botões de adicionar ao carrinho
+const botoesBordados = document.querySelectorAll('.btn-adicionar')
+
+//Agora vamos criar o carrinho de compras
+let carrinho = JSON.parse(localStorage.getItem('itemCarrinho')) || []
+
+//Função para adicionar itens ao carrinho
+botoesBordados.forEach(function(botao){
+    botao.addEventListener('click', function(){
+        event.preventDefault()
+        //Evita  que a página recarregue
+
+        //Recebe dados do botão
+        const nomeProduto = botao.getAttribute('data-nome')
+        const precoProduto = botao.getAttribute('data-preco')
+
+        //Cria um objeto para o item do carrinho
+        const itemCarrinho = {
+            nome: nomeProduto,
+            preco: precoProduto
+        }
+
+        //Adiciona o item ao carrinho
+        carrinho.push(itemCarrinho)
+
+        //Salva o carrinho no localStorage
+        localStorage.setItem('itemCarrinho', JSON.stringify(carrinho))
+
+        //Exibe uma mensagem de confirmação
+        botao.innerText = 'Adicionado!'
+        botao.classList.replace('btn-primary', 'btn-success')
+
+        //Depois de um tempo a mensagem some
+        setTimeout(() => {
+            botao.innerText = 'Adicionar ao <i class="fa-solid fa-cart-shopping"></i>'
+            botao.classList.replace('btn-success', 'btn-primary')}, 2000)
+        })
+})
